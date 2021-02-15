fc0679686cfd1507c8b9e6ab4546d08ebb1841fb
najahou <najah.ouami@gmail.com>
2021-02-15 11:24:57 +0100

    first

---
+++ src/main/java/com/SmartHopes/SmartHopes/SmartHopesApplication.java
@@ -0,0 +1,13 @@
+package com.SmartHopes.SmartHopes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHopesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHopesApplication.class, args);
+	}
+
+}
