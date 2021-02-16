7c876f98086534ac7f05101032cfd024a305770d
najahou <najah.ouami@gmail.com>
2021-02-16 22:49:07 +0100

    edit-najah

--- src/main/java/com/SmartHopes/SmartHopes/SmartHopesApplication.java
+++ src/main/java/com/SmartHopes/SmartHopes/SmartHopesApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmartHopesApplication {
-
+public class SmartHopesApplication
+	{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SmartHopesApplication.class, args);
+		System.out.println("hello");
+		
 	}
+
 
 }
--- src/test/java/com/SmartHopes/SmartHopes/SmartHopesApplicationTests.java
+++ src/test/java/com/SmartHopes/SmartHopes/SmartHopesApplicationTests.java
@@ -1,13 +1,28 @@
 package com.SmartHopes.SmartHopes;
 
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import Entities.Produit;
+import Repositories.ProduitRepository;
 
 @SpringBootTest
 class SmartHopesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired 
+	private ProduitRepository produitRepository; 
+
+	@Test 
+
+	public void testCreateProduit() { 
+
+	Produit prod = new Produit("p1",452.250); 
+
+	produitRepository.save(prod); 
+
+	} 
 
 }
