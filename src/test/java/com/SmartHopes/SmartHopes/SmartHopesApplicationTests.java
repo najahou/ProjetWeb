6202667e4c15c76fa8e6e476dfe457775d468fb9
najahou <78212136+najahou@users.noreply.github.com>
2021-02-17 01:07:01 +0100

    Update SmartHopesApplicationTests.java

--- src/test/java/com/SmartHopes/SmartHopes/SmartHopesApplicationTests.java
+++ src/test/java/com/SmartHopes/SmartHopes/SmartHopesApplicationTests.java
@@ -6,23 +6,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import Entities.Produit;
-import Repositories.ProduitRepository;
 
 @SpringBootTest
 class SmartHopesApplicationTests {
 
-	@Autowired 
-	private ProduitRepository produitRepository; 
-
-	@Test 
-
-	public void testCreateProduit() { 
-
-	Produit prod = new Produit("p1",452.250); 
-
-	produitRepository.save(prod); 
-
+	
 	} 
 
 }
